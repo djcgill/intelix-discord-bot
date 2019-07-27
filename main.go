37afcafc1834adf81f412f44620b49f85e2b7e00
djcg <daniel_gilliam@hotmail.com>
2019-07-27 21:38:29 +0100

    Added function to calculate SHA256 of attachments

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
-	"os"
 	"fmt"
+	"net/http"
+	"io/ioutil" 
 	"log"
+	"os"
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -51,8 +55,40 @@ func scanAttachments(s*discordgo.Session, msg*discordgo.MessageCreate) {
 		// Don't check the bots own messages
 		return
 	}
+
+	attatchments := msg.Attachments
+	if attatchments != nil {
+		for _, atch := range attatchments {
+			sha256, err := getSHA(atch.URL)
+			if err != nil {
+				log.Println("Problem determining SHA256 skipping...")
+			}
+			fmt.Println(sha256)
+		}
+	}
+	
+
 	content := msg.Content
 	s.ChannelMessageSend(msg.ChannelID, content)
+}
+
+func getSHA(FileUrl string) (string, error) {
+	resp, err := http.Get(FileUrl)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	h := sha256.New()
+	h.Write([]byte(body))
+	hash := hex.EncodeToString(h.Sum(nil))
+
+	return hash, err
 }
 
 func loadConfig(file string) Configuration {
